ce653729d534c4d9cb9f8196eadca7e3d91d59ed
Lucas Rafaell <lucas.rafaell@outloo.com>
2022-04-23 14:33:12 -0300

    chore(DI) - insert application dependecies in crosscutting boot strapper injector

--- src/SiegeInitiative.CrossCutting/IoC/NativeInjectorBootStrapper.cs
+++ src/SiegeInitiative.CrossCutting/IoC/NativeInjectorBootStrapper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SiegeInitiative.Application.Extensions;
 using SiegeInitiative.Infrastructure.Persistence.Extensions;
 
 namespace SiegeInitiative.CrossCutting.IoC;
@@ -8,6 +9,9 @@ public static class NativeInjectorBootStrapper
 {
     public static IServiceCollection RegisterAplicationDependencies(this IServiceCollection services, IConfiguration configuration)
     {
+        // Application
+        services.AddApplicationDependecies(configuration);
+
         // Persistence
         services.AddPersistenceDependencies(configuration);
 
---
+++ src/SiegeInitiative.Domain/Entities/Team.cs
@@ -0,0 +1,8 @@
+﻿using SiegeInitiative.Domain.Entities.Base;
+
+namespace SiegeInitiative.Domain.Entities;
+
+public sealed class Team : AggregateRoot<int>
+{
+
+}
