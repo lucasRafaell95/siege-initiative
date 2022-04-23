790af37106bd83ee7970b5a563d518b4f9de2c2f
Lucas Rafaell <lucas.rafaell@outloo.com>
2022-04-23 10:12:00 -0300

    feat(api) - Health check configuration in the API layer

---
+++ src/SiegeInitiative.Api/Extensions/ApplicationBuilderExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SiegeInitiative.Api.Extensions;
+
+public static class ApplicationBuilderExtension
+{
+    public static IApplicationBuilder UseHealthCheck(this IApplicationBuilder app)
+    {
+        app.UseHealthChecks("/health");
+
+        app.UseHealthChecks("/health/details", new HealthCheckOptions()
+        {
+            ResponseWriter = (httpContext, result) =>
+            {
+                httpContext.Response.ContentType = "application/json";
+
+                var json = new JObject(
+                    new JProperty("status", result.Status.ToString()),
+                    new JProperty("results", new JObject(result.Entries.Select(pair =>
+                        new JProperty(pair.Key, new JObject(
+                            new JProperty("status", pair.Value.Status.ToString()),
+                            new JProperty("description", pair.Value.Description),
+                            new JProperty("data", new JObject(pair.Value.Data.Select(
+                                p => new JProperty(p.Key, p.Value))))))))));
+                return httpContext.Response.WriteAsync(json.ToString(Formatting.Indented));
+            }
+        });
+
+        return app;
+    }
+}
---
+++ src/SiegeInitiative.Api/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace SiegeInitiative.Api.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApiDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSwagger();
+
+        services.AddHealthCheck(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddSwagger(this IServiceCollection services)
+        => services.AddSwaggerGen();
+
+    private static IServiceCollection AddHealthCheck(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("SiegeInitiative"));
+
+        return services;
+    }
+}
