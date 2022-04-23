4d85b316270e9e93be377b6865eb02dd0723c654
Lucas Rafaell <lucas.rafaell@outloo.com>
2022-04-23 14:32:09 -0300

    feat(caching) - implementation of basic redis bache service

---
+++ src/SiegeInitiative.Application/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SiegeInitiative.Application.Caching.Base;
+using SiegeInitiative.Application.Options;
+using SiegeInitiative.Application.Options.Base;
+
+namespace SiegeInitiative.Application.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApplicationDependecies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDistributedCache(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddDistributedCache(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddSingleton<IRedisCacheOptions>(_ => configuration.GetSection("DistributedCache").Get<RedisCacheOptions>());
+
+        services.AddCacheServices();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCacheServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRedisCacheService<,>), typeof(RedisCacheService<,>));
+
+        return services;
+    }
+}
---
+++ src/SiegeInitiative.Application/Options/RedisCacheOptions.cs
@@ -0,0 +1,21 @@
+﻿using SiegeInitiative.Application.Options.Base;
+
+namespace SiegeInitiative.Application.Options;
+
+public sealed record RedisCacheOptions : IRedisCacheOptions
+{
+    /// <summary>
+    /// Cache enable flag
+    /// </summary>
+    public bool Enabled { get; init; }
+
+    /// <summary>
+    /// Distributed Cache Service Connection String
+    /// </summary>
+    public string ConnectionString { get; init; }
+
+    /// <summary>
+    /// Cache service registration expiration time
+    /// </summary>
+    public TimeSpan ExpirationTime { get; init; }
+}
